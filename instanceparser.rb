267874ddcd72d1aeac8f97115abf709f24ee4bd9
Robson Dornelles <dudassdornelles@gmail.com>
2010-04-26 11:43:06 -0300

    updated instance parser

--- instanceparser.rb
+++ instanceparser.rb
@@ -6,12 +6,12 @@ class InstanceParser
 	end
 
 	def find_instances 
-		#a unix grep does this with 'grep -Rn entity *'
+		#a unix grep does this with `grep -Rn " #{@entity} " *`
 		puts "ATTENTION: You must update the following instances of #{@entity.upcase}"
 		Dir['*.vhd'].each do |file|
 			i = 1
 			open(file).each_line do |line|
-				if not line.grep(/\S+:\s+#{@entity}\s+/).empty?
+				if line =~ /\s*\S+:\s+#{@entity}\s+/i
 					puts "in " + file + " line "+ i.to_s + ":  " + line
 				end
 				i += 1
@@ -20,4 +20,7 @@ class InstanceParser
 	end
 	
 end
+
+e = InstanceParser.new ARGV[0]
+e.find_instances
 
